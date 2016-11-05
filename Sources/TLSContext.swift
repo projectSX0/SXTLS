65414082003d70c19c1aacc6b666677da5e0a4b3
yuji <hatsuneyuji@yahoo.co.jp>
2016-11-04 18:05:06 -0700

    update

--- Sources/TLSContext.swift
+++ Sources/TLSContext.swift
@@ -1,47 +1,61 @@
-
-import swiftTLS
-import spartanX
-
-import struct Foundation.Data
-public typealias SXTLSContext = TLSServer
-public typealias CString = UnsafePointer<Int8>
-
-public class SXTLSLayer :  SXStreamSocketService {
-    
-    public var context: TLSServer
-    public var dataHandler: (SXQueue, Data) -> Bool
-    public var errHandler: ((SXQueue, Error) -> ())?
-    public var acceptedHandler: ((inout SXClientSocket) -> ())?
-    
-    public var clientsMap = [Int32: TLSClient]()
-    
-    public init(service: SXService, context: TLSServer) {
-        
-        self.context = context
-        
-        self.dataHandler = service.dataHandler
-        self.errHandler = { queue, error in
-            switch error {
-            case TLSError.filedescriptorNotWriteable, TLSError.filedescriptorNotReadable:
-                break
-            default:
-                service.errHandler?(queue, error)
-            }
-        }
-        
-        self.acceptedHandler = { client in
-            do {
-                self.clientsMap[client.sockfd] = try self.context.accept(socket: client.sockfd)
-                client.readMethod = { client_socket throws -> Data? in
-                    return try self.clientsMap[client_socket.sockfd]?.read(size: 16 * 1024)
-                }
-                
-                client.writeMethod = { client_socket, data throws in
-                    _ = try self.clientsMap[client_socket.sockfd]?.write(data: data)
-                }
-            } catch {
-                
-            }
-        }
-    }
-}
+//
+//import swiftTLS
+//import spartanX
+//
+//import struct Foundation.Data
+//
+//public class SXTLSLayer {
+//    public var supportingMethods: SendMethods = [.send]
+//    var service: SXStreamService
+//    var servContext: SXTLSContext
+//    
+//    public init(service: SXStreamService, context: SXTLSContext) {
+//        self.service = service
+//        self.servContext = context
+//    }
+//}
+//
+//extension TLSClient : Readable, Writable {
+//    
+//    public var availableBytesa: Int {
+//        return 4096
+//    }
+//
+//    public func write(data: Data) throws {
+//        return try self.write(data: data)
+//    }
+//    
+//    public func read(size: Int) throws -> Data? {
+//        return try self.read(size: size)
+//    }
+//    
+//    public func done() {
+//        self.close()
+//    }
+//}
+//
+//extension SXTLSLayer {
+//    public func received(data: Data, from connection: SXQueue) throws -> Bool {
+//        return try service.recevied(data: data, from: connection)
+//    }
+//    
+//    public func exceptionRaised(_ exception: Error, on connection: SXQueue) {
+//        if let error = exception as? TLSError {
+//            switch error {
+//            case TLSError.filedescriptorNotWriteable, TLSError.filedescriptorNotReadable:
+//                return
+//            default:
+//                break
+//            }
+//        }
+//        
+//        self.service.exceptionRaised(exception, on: connection)
+//    }
+//    
+//    public func accpeted(socket: SXClientSocket, as connection: SXQueue) throws {
+//        let client_context = try self.servContext.accept(socket: socket.sockfd)
+//        connection.userInfo["tls"] = client_context
+//        connection.readAgent = connection.userInfo["tls"] as! TLSClient
+//        connection.writeAgent = connection.userInfo["tls"] as! TLSClient
+//    }
+//}
--- Sources/spartanXTLS.swift
+++ Sources/spartanXTLS.swift
@@ -1,4 +1,80 @@
-struct spartanXTLS {
 
-    var text = "Hello, World!"
+import swiftTLS
+import spartanX
+
+import struct Foundation.Data
+
+public typealias SXTLSContext = TLSServer
+public typealias CString = UnsafePointer<Int8>
+public typealias SXConnection = SXQueue
+
+public class SXTLSLayer {
+    public var supportingMethods: SendMethods = [.send]
+    var service: SXService
+    var servContext: SXTLSContext
+    
+    public init(service: SXService, context: SXTLSContext) {
+        self.service = service
+        self.servContext = context
+    }
+}
+
+extension TLSClient : Readable, Writable {
+    
+    public var availableBytesa: Int {
+        return 4096
+    }
+    
+    public func write(data: Data) throws {
+        return try self.write(data: data)
+    }
+    
+    public func read(size: Int) throws -> Data? {
+        return try self.read(size: size)
+    }
+    
+    public func done() {
+        self.close()
+    }
+}
+
+extension SXTLSLayer : SXStreamService {
+    
+    public func received(data: Data, from connection: SXConnection) throws -> Bool {
+        return try service.received(data: data, from: connection)
+    }
+    
+    public func exceptionRaised(_ exception: Error, on connection: SXQueue) {
+        if let error = exception as? TLSError {
+            switch error {
+            case TLSError.filedescriptorNotWriteable, TLSError.filedescriptorNotReadable:
+                return
+            default:
+                break
+            }
+        }
+        
+        self.service.exceptionRaised(exception, on: connection)
+    }
+    
+    public func accepted(socket: SXClientSocket, as connection: SXConnection) throws {
+        let client_context = try self.servContext.accept(socket: socket.sockfd)
+        connection.userInfo["tls"] = client_context
+        connection.readAgent = connection.userInfo["tls"] as! TLSClient
+        connection.writeAgent = connection.userInfo["tls"] as! TLSClient
+    }
+    
+    public func connectionWillTerminate(_ connection: SXQueue) {
+        guard let service = service as? SXStreamService else {
+            return
+        }
+        service.connectionWillTerminate(connection)
+    }
+    
+    public func connectionDidTerminate(_ connection: SXQueue) {
+        guard let service = service as? SXStreamService else {
+            return
+        }
+        service.connectionDidTerminate(connection)
+    }
 }
